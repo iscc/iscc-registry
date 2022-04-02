3273b83fdd398317f0c35c934fa1d70fc7ea25fd
Titusz Pan <titusz.pan@gmail.com>
2022-04-02 18:28:33 +0200

    add .env support and installer script

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ based on the ISCC declaration protocol.*
 ## Development Status
 
 Under development ...
+
+## Develoment Setup
+
+**Requirements:**
+
+- [Python](https://www.python.org/) 3.8 - 3.10
+- [Poetry](https://python-poetry.org/)
+
+Get up and running:
+```shell
+git clone https://github.com/iscc/iscc-registry.git
+cd iscc-registry
+poetry install
+poe demo
+python manage.py runserver
+```
+
---
+++ dev/demo.py
@@ -0,0 +1,47 @@
+"""Initialize a fresh installation
+
+- creates the database
+- import theme fixtures
+- create demo user
+
+"""
+import sys
+
+from loguru import logger as log
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iscc_registry.settings")
+from django.contrib.auth import get_user_model
+from django.core import management
+import django
+import pathlib
+
+
+HERE = pathlib.Path(__file__).parent.absolute()
+django.setup()
+
+
+def demo():
+    dev_db = HERE / "dev.db"
+    if dev_db.exists():
+        log.info(f"deleting dev database at {dev_db}")
+        try:
+            os.remove(HERE / "dev.db")
+        except Exception:
+            log.error(f"failed deleting dev database - retry after stopping dev server")
+            sys.exit(0)
+    log.info("applying database migrations")
+    management.call_command("migrate")
+
+    log.info("creating demo user")
+    User = get_user_model()
+    username = "demo"
+    password = "demo"
+    email = "demo@eexample.com"
+    User.objects.create_superuser(username=username, password=password, email=email)
+    log.info(f"Username: {username}")
+    log.info(f"Password: {password}")
+
+
+if __name__ == "__main__":
+    demo()
