34052bb796de34626da23602210a80235ac075e4
Titusz Pan <titusz.pan@gmail.com>
2022-04-02 16:38:10 +0200

    add default django boilerplate

---
+++ iscc_registry/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for iscc_registry project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iscc_registry.settings')
+
+application = get_asgi_application()
---
+++ iscc_registry/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for iscc_registry project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iscc_registry.settings')
+
+application = get_wsgi_application()
