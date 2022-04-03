5ab139e1970de718f2534b5ee14f422f23fd6f0d
Titusz Pan <titusz.pan@gmail.com>
2022-04-03 23:48:06 +0200

    use poetry shell

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"] = "sqlite:///./dev/test.db"
+os.environ["DATABASE_URL"] = "sqlite://:memory:"
 os.environ["SECRET_KEY"] = "test-secret"
 from iscc_registry.settings import *
