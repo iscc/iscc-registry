08281aa4821b0b9da3bd702a03cc3ac2772296ca
Titusz Pan <titusz.pan@gmail.com>
2022-04-11 10:30:40 +0200

    add fake data generator and loader

--- dev/fake.py
+++ dev/fake.py
@@ -1,10 +1,13 @@
 """Fake data generator for testing"""
+import iscc_registry.init
 import iscc_core as ic
 import random
 from blake3 import blake3
+from iscc_registry.schema import Declaration
+from iscc_registry.transactions import register
 
 
-random.seed(1)
+rnd = random.Random(0)
 
 
 class classproperty(object):
@@ -22,22 +25,32 @@ class Fake:
     CHAIN_ID = 1
 
     @classproperty
-    def time(cls):
-        cls.TIME += random.randint(1, 1000)
+    def timestamp(cls):
+        cls.TIME += rnd.randint(1, 1000)
         return cls.TIME
 
     @classproperty
     def iscc_id(cls):
-        return ic.Code.rnd(mt=ic.MT.ID)
+        return ic.Code.rnd(mt=ic.MT.ID).code
 
     @classproperty
     def iscc_code(cls):
-        return ic.Code.rnd(mt=ic.MT.ISCC, bits=256)
+        return ic.Code.rnd(mt=ic.MT.ISCC, bits=256).code
 
     @classproperty
     def chain_id(cls):
-        cls.CHAIN_ID = random.choice(list(ic.ST_ID)).value
+        cls.CHAIN_ID = rnd.choice(list(ic.ST_ID)).value
         return cls.CHAIN_ID
+
+    @classproperty
+    def tx_idx(cls):
+        return rnd.randint(0, 3000)
+
+    @classproperty
+    def tx_hash(cls):
+        bits = 32 * 8
+        data = rnd.getrandbits(bits).to_bytes(length=bits // 8, byteorder="big")
+        return "0x" + data.hex()
 
     @classproperty
     def chain_name(cls):
@@ -46,30 +59,61 @@ class Fake:
     @classproperty
     def wallet(cls):
         bits = 20 * 8
-        data = random.getrandbits(bits).to_bytes(length=bits // 8, byteorder="big")
+        data = rnd.getrandbits(bits).to_bytes(length=bits // 8, byteorder="big")
         return "0x" + data.hex()
 
     @classproperty
     def block_hash(cls):
-        bits = 30 * 8
         return "0x" + blake3(str(cls.block_height).encode()).hexdigest()
 
     @classproperty
     def block_height(cls):
-        cls.BLOCK_HEIGHT[cls.chain_name] += random.randint(1, 1000)
+        cls.BLOCK_HEIGHT[cls.chain_name] += rnd.randint(1, 1000)
         return cls.BLOCK_HEIGHT[cls.chain_name]
+
+    @staticmethod
+    def rbytes(n: int):
+        """Random bytes"""
+        return rnd.getrandbits(n * 8).to_bytes(length=n, byteorder="big")
+
+    @classproperty
+    def meta_url(cls):
+        base = "b"
+        codec = b"\x01"
+        ctype = b"\x55"
+        htype = b"\x12"
+        data = codec + ctype + htype + cls.rbytes(32)
+        cid = base + ic.encode_base32(data).lower()
+        return f"ipfs://{cid}"
+
+    @classproperty
+    def declaration(cls):
+        return Declaration(
+            timestamp=cls.timestamp,
+            chain_id=cls.chain_id,
+            block_height=cls.block_height,
+            block_hash=cls.block_hash,
+            tx_idx=cls.tx_idx,
+            tx_hash=cls.tx_hash,
+            declarer=cls.wallet,
+            iscc_code=cls.iscc_code,
+            message=rnd.choice(("frz:", "del:", None, None, None, None, None, None, None, None)),
+            meta_url=rnd.choice((None, cls.meta_url)),
+            registrar=rnd.choice((None, cls.wallet)),
+        )
 
 
 def main():
-    print(Fake.time)
-    print(Fake.iscc_id)
-    print(Fake.iscc_code)
-    print(Fake.chain_name)
-    print(Fake.chain_id)
-    print(Fake.wallet)
-    print(Fake.block_hash)
-    print(Fake.block_height)
+    from pprint import pp
+
+    for x in range(10):
+        pp(Fake.declaration.dict())
+
+
+def load(n):
+    for x in range(n):
+        register(Fake.declaration)
 
 
 if __name__ == "__main__":
-    main()
+    load(1000)
--- dev/load.py
+++ dev/load.py
@@ -2,51 +2,14 @@
 import iscc_registry.init
 from iscc_registry import schema
 from iscc_registry.transactions import register
+from dev.fake import Fake
 
 
-def load():
-    a = schema.Declaration(
-        timestamp=1649008119,
-        chain_id=2,
-        block_height=14514543,
-        block_hash="0xaa735e41758bd8f411117ac7f20ef3779c35ab9c9c2e4f5c70c87d4d73979f05",
-        tx_idx=0,
-        tx_hash="0xaade12c2cba31fbbfeddd1df932388dcd1c43fa346e233e34915dc3694546f3a",
-        declarer="0x1ad91ee08f21be3de0ba2ba6918e714da6b45836",
-        iscc_code="KACT4EBWK27737D2AYCJRAL5Z36G76RFRMO4554RU26HZ4ORJGIVHDI",
-    )
-    iscc_id_obj_a = register(a)
-
-    b = schema.Declaration(
-        timestamp=1649009000,
-        chain_id=2,
-        block_height=14514544,
-        block_hash="0xbb735e41758bd8f411117ac7f20ef3779c35ab9c9c2e4f5c70c87d4d73979f06",
-        tx_idx=0,
-        tx_hash="0xbbde12c2cba31fbbfeddd1df932388dcd1c43fa346e233e34915dc3694546f3b",
-        declarer="0x1ad91ee08f21be3de0ba2ba6918e714da6b45836",
-        iscc_code="KACT4EBWK27737D2AYCJRAL5Z36G76RFRMO4554RU26HZ4ORJGIVHDI",
-        meta_url="ipfs://bafybeihcck6iocb2steuf4zwq53nfyce34xamke5za7gaq2qqoshmgab6u",
-    )
-    iscc_id_obj_b = register(b)
-
-    c = schema.Declaration(
-        timestamp=1649009900,
-        chain_id=2,
-        block_height=14514544,
-        block_hash="0xbb735e41758bd8f411117ac7f20ef3779c35ab9c9c2e4f5c70c87d4d73979f06",
-        tx_idx=0,
-        tx_hash="0xccde12c2cba31fbbfeddd1df932388dcd1c43fa346e233e34915dc3694546f3b",
-        declarer="0x1bd91ee08f21be3de0ba2ba6918e714da6b45836",
-        iscc_code="KACT4EBWK27737D2AYCJRAL5Z36G76RFRMO4554RU26HZ4ORJGIVHDI",
-    )
-
-    iscc_id_obj_c = register(c)
-
-    print(iscc_id_obj_a)
-    print(iscc_id_obj_b)
-    print(iscc_id_obj_c)
+def load(n):
+    for _ in range(n):
+        obj = register(Fake.declaration)
+        print(obj)
 
 
 if __name__ == "__main__":
-    load()
+    load(1000)
