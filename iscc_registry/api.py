41acf2795f479bb190f2219ac673541c36d9a33e
Titusz Pan <titusz.pan@gmail.com>
2022-04-11 20:46:10 +0200

    add head, register, rollback observer api endpoints

--- iscc_registry/api.py
+++ iscc_registry/api.py
@@ -11,9 +11,13 @@
 from django.shortcuts import redirect
 from ninja import NinjaAPI
 from ninja.errors import HttpError
-
+from iscc_registry.exceptions import RegistrationError
 from iscc_registry import schema as s
 import iscc_core as ic
+from iscc_registry.models import IsccIdModel
+from iscc_registry.schema import Head, Message, RegistrationResponse, Declaration
+from iscc_registry.transactions import rollback, register
+
 
 api = NinjaAPI(
     title="ISCC - Registry API",
@@ -28,6 +32,30 @@ def index(request):
     return {}
 
 
+@api.get("/head/{chain_id}", tags=["observer"], response={200: Head, 404: Message})
+def head(request, chain_id: int):
+    """Return block header of the latest registration event for given chain."""
+    obj = IsccIdModel.objects.filter(chain_id=chain_id).order_by("did").last()
+    if not obj:
+        return 404, Message(message="No registrations found for chain")
+    return 200, obj
+
+
+@api.post("/register", tags=["observer"], response={201: RegistrationResponse, 422: Message})
+def register_(request, declartion: Declaration):
+    """Register an on-chain ISCC-Declaration for ISCC-ID minting."""
+    try:
+        return 201, register(declartion)
+    except RegistrationError as e:
+        return 422, Message(message=str(e))
+
+
+@api.post("/rollback/{block_hash}", tags=["observer"], response={200: Head, 404: Message})
+def rollback_(request, block_hash: str):
+    """Rollback events to state before `block_hash`"""
+    return rollback(block_hash)
+
+
 @api.get("/resolve/{iscc}", tags=["public"])
 def resolve(request, iscc: str):
     try:
@@ -36,9 +64,3 @@ def resolve(request, iscc: str):
         raise HttpError(400, str(e))
 
     return redirect("https://example.com", permanent=False)
-
-
-@api.post("/register", tags=["observer"], response=s.DeclarationResponse)
-def register(request, declartion: s.Declaration):
-    """Register an on-chain ISCC-Declaration for ISCC-ID minting."""
-    pass
