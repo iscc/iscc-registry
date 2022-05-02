6f81c0437b99b90db8911c77b0b94527ce554c71
Titusz Pan <titusz.pan@gmail.com>
2022-05-02 11:42:10 +0200

    redirect intermediate navigation

--- iscc_registry/urls.py
+++ iscc_registry/urls.py
@@ -9,6 +9,8 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path("api/v1/", api_v1.urls, name="api-v1"),
     path("dashboard/", admin.site.urls, name="dashboard"),
+    path("registry/", RedirectView.as_view(url="/registry/iscc_registry/isccid/")),
+    path("registry/iscc_registry/", RedirectView.as_view(url="/registry/iscc_registry/isccid/")),
     path("registry/", public_admin.urls, name="registry"),
     path("<str:iscc_id>/", views.resolver, name="resolver"),
     path("", RedirectView.as_view(url="/registry/iscc_registry/isccid/"), name="index"),
